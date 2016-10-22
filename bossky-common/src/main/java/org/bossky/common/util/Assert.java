49c2d102c8d5b7f02aaddd7699ff87bf34fe02ac
daibo <daibo@5ab4f599-ba13-4ab6-ab20-c5c0a7caf04b>
2016-10-22 17:44:31 +0000

    增加时间类

--- bossky-common/src/main/java/org/bossky/common/util/Assert.java
+++ bossky-common/src/main/java/org/bossky/common/util/Assert.java
@@ -8,7 +8,10 @@ package org.bossky.common.util;
  */
 public class Assert {
 
-	public static String DEFAULT_ASSERT_NOT_NULL_TOP = "参数不能为空";
+	private static String DEFAULT_ASSERT_NOT_NULL_TOP = "参数不能为空";
+	private static String DEFAULT_ASSERT_NULL_TOP = "参数必须为空";
+	private static String DEFAULT_ASSERT_EQ = "对象必须相等";
+	private static String DEFAULT_ASSERT_NOT_EQ = "对象必须不相等";
 
 	/**
 	 * 断言对象非空
@@ -32,4 +35,69 @@ public class Assert {
 			throw new IllegalArgumentException(message);
 		}
 	}
+
+	/**
+	 * 断言对象是空
+	 * 
+	 * @param obj
+	 */
+	public static void assertNull(Object obj) {
+		assertNull(obj, DEFAULT_ASSERT_NULL_TOP);
+	}
+
+	/**
+	 * 断言对象是空
+	 * 
+	 * @param obj
+	 */
+	public static void assertNull(Object obj, String message) {
+		if (null == obj) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	/**
+	 * 断言对象相等
+	 * 
+	 * @param obj
+	 * @param other
+	 */
+	public static void assertEq(Object obj, Object other) {
+		assertEq(obj, other, DEFAULT_ASSERT_EQ);
+	}
+
+	/**
+	 * 断言对象相等
+	 * 
+	 * @param obj
+	 * @param other
+	 */
+	public static void assertEq(Object obj, Object other, String message) {
+		if (!Misc.eq(obj, other)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	/**
+	 * 断言对象不相等
+	 * 
+	 * @param obj
+	 * @param other
+	 */
+	public static void assertNotEq(Object obj, Object other) {
+		assertEq(obj, other, DEFAULT_ASSERT_NOT_EQ);
+	}
+
+	/**
+	 * 断言对象不相等
+	 * 
+	 * @param obj
+	 * @param other
+	 */
+	public static void assertNotEq(Object obj, Object other, String message) {
+		if (!Misc.eq(obj, other)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
 }
